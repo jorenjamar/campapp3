955112a7184f472ec3bdbc49ec5d64650a3c9273
Joren Jamar <jorenjamar@hotmail.com>
2018-07-25 11:48:01 +0200

    opbouw en contact activity aangemaakt en openen van deze activities via buttons op de main activity

---
+++ app/src/main/java/com/example/joren/campapp3/ContactActivity.kt
@@ -0,0 +1,12 @@
+package com.example.joren.campapp3
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ContactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact)
+    }
+}
--- app/src/main/java/com/example/joren/campapp3/MainActivity.kt
+++ app/src/main/java/com/example/joren/campapp3/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.joren.campapp3
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOpbouw.setOnClickListener{
+            val intent = Intent(this, OpbouwActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnContact.setOnClickListener{
+            val intent = Intent(this, ContactActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
